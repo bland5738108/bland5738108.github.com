4175195bd9a333e57f924946ba3f7a8e4c99d6b8
bland <cqj5738108@sina.com>
2017-05-04 16:41:10 +0800

    ccc

--- move/移动页面/js/change.js
+++
@@ -1,7 +0,0 @@
-;(function(win,doc){
-	function change(){
-		doc.documentElement.style.fontSize = 20*doc.documentElement.clientWidth/375+'px';
-	}
-	change();
-	win.addEventListener('resize',change,false);
-})(window,document);
